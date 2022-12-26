30e359655c218772705aefe70b71b3099b947815
masonmike2001 <masonmike2001@gmail.com>
2022-12-26 14:46:50 -0500

    creates basic structure of to do list page, including input forms

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { compareAsc, format } from 'date-fns';
+
+/* Features:
+ * populate categories, CRUD
+ * populate todos, CRUD
+ * assign priorities to todos
+ * change border color with priority, background color with project
+*/
+
+onPageLoad();
+
+/* 
+ * 
+*/ 
+function onPageLoad() {
+
+}
